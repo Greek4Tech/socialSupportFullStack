0701e3f2b0a238fd553603b90d74e0d653475b95
Greek4Tech <97710680+Greek4Tech@users.noreply.github.com>
2022-10-09 13:58:19 -0700

    more dependencies

--- server.js
+++ server.js
@@ -6,3 +6,31 @@ const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
 const flash = require('express-flash');
 const logger = require('morgan');
+const connectDB = require('./config/database');
+// Routes 
+const mainRoutes = require('./routes/main');
+const socials = require('./routes/socials');
+
+require('dotenv').config({ path: './config/.env' });
+
+// Passport for authentication
+require('./config/passport')(passport);
+
+connectDB();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(logger('dev'));
+
+// Sessions. This needs to go BEFORE live app.use(passport.initialize());
+app.use( 
+    session({
+      //makes sure you have a DB connection
+      secret: 'keyboard cat', //used to compute the hash - can be changed
+      resave: false,
+      saveUninitialized: false,
+      store: new MongoStore({ mongooseConnection: mongoose.connection }),
+    })
+  );
