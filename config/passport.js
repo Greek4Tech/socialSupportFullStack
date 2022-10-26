dca75eb2e72e087c312b2b5724c015a6de30be67
Greek4Tech <97710680+Greek4Tech@users.noreply.github.com>
2022-10-26 15:48:36 -0500

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -26,7 +26,7 @@ module.exports = function (passport) {
   passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: '//never-alone.up.railway.app/'
+    callbackURL: '/auth/google/callback'
 },
 async (accessToken, refreshToken, profile, done) => {
    const newUser = {
